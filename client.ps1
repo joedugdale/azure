29352cabc9dc3fbc5e27f8195121353adc047909
LAPTOP-FQAP5KV2\Joe <joedugdale@gmail.com>
2017-09-20 14:52:42 +1000

    add files - jd

---
+++ client.ps1
@@ -0,0 +1,14 @@
+﻿$uri = "https://s8events.azure-automation.net/webhooks?token=Dt%2fuh6WrXP06FNGeXBxZfPqXqC%2boPQu98gziZjKhJoM%3d"
+$headers = @{"From"="user@contoso.com";"Date"="09/20/2017 15:47:00"}
+
+$myvars  = @(
+    @{AzureResourceGroup="Mocatad_EV17_AS";Shutdown="true" }
+)
+
+$body = ConvertTo-Json -InputObject $myvars 
+
+$response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
+
+Write-Output $response.JobIds 
+
+
